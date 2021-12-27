5206a881e290c5615b667d1856a0929e43cfe96b
riccardoEspositoXhpo <espositoriccardo101@gmail.com>
2021-12-27 15:51:51 +0100

    complete module 1

---
+++ module01/ThreeSort.java
@@ -0,0 +1,17 @@
+public class ThreeSort {
+    public static void main(String[] args) {
+        
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+
+        int first = Math.min(a, Math.min(b, c));
+        int third = Math.max(a, Math.max(b, c));
+
+        // the middle number is the one left-over after subtracting first and second
+        int second = (a + b + c) - (first + third);
+
+
+        System.out.println("Numbers in order: " + first + ", " + second + ", " + third);
+    }
+}
---
+++ module01/Uniform.java
@@ -0,0 +1,15 @@
+public class Uniform {
+    public static void main(String[] args) {
+        double a = Math.random();
+        double b = Math.random();
+        double c = Math.random();
+        double d = Math.random();
+        double e = Math.random();
+        double average = ( a + b + c + d + e ) / 5;
+
+        System.out.println("Numbers are: " + a + ", " + b + ", " + c + ", " + d + ", " + e);
+        System.out.println("Max is: " + Math.max(a, Math.max(b, Math.max(c, Math.max(d, e)))));
+        System.out.println("Min is: " + Math.min(a, Math.min(b, Math.min(c, Math.min(d, e)))));
+        System.out.println("Average is: " + average);
+    }
+}
